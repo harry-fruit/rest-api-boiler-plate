f80d472f345089e90fb9e4c0fdbdddb11b312f9c
Isaque D <isaqueduarte17@gmail.com>
2024-10-03 12:26:56 -0300

    chore: update mock helpers

--- src/__mocks__/mockHelper.ts
+++ src/__mocks__/mockHelper.ts
@@ -1,11 +1,10 @@
 import { getDBsConfigs } from '../configs/__mocks__/configurations';
 import { newDatabaseInstance } from '../database/__mocks__/database';
-import logger from '../utils/__mocks__/logger';
-import { AppRouter } from './router';
 
+
+//TODO: Refazer helpers
 // Configuração padrão dos mocks
 export const setupMocks = () => {
-  // Mock getDBsConfigs
   getDBsConfigs.mockResolvedValue([
     { dbType: 'postgres', schema: 'public', orm: 'typeorm', dbName: 'test_db' },
   ]);
